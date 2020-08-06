3de541599096255cfa506747f73e633ec9f399d8
Felix Biego <45796740+fbiego@users.noreply.github.com>
2020-08-06 19:17:12 +0300

    Create dt78_ble_server.ino

---
+++ dt78_ble_server/dt78_ble_server.ino
@@ -0,0 +1,115 @@
+/*
+ * This sketch creates a BLE server on the ESP32 board that emulates the characteristics and services of the DT78 watch
+ * this will enable the ESP32 connect to the WearFit 2.0 app
+ * 
+ */
+
+#include <BBUILTINLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include <BLE2902.h>
+
+#define BUILTINLED 2
+
+uint8_t bat80[] = {0xAB, 0x00, 0x05, 0xFF, 0x91, 0x80, 0x00, 0x50}; //bat 80%
+
+#define SERVICE_UUID        "6e400001-b5a3-f393-e0a9-e50e24dcca9e"
+#define CHARACTERISTIC_UUID_RX "6e400002-b5a3-f393-e0a9-e50e24dcca9e"
+#define CHARACTERISTIC_UUID_TX "6e400003-b5a3-f393-e0a9-e50e24dcca9e"
+
+static BLECharacteristic* pCharacteristicTX;
+static BLECharacteristic* pCharacteristicRX;
+static bool deviceConnected = false;
+
+class MyServerCallbacks: public BLEServerCallbacks{
+  void onConnect(BLEServer* pServer){
+    deviceConnected = true;
+  }
+  void onDisconnect(BLEServer* pServer){
+    deviceConnected = false;
+  }
+};
+
+class MyCallbacks: public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic *pCharacteristic){
+    uint8_t* pData;
+    std::string value = pCharacteristic->getValue();
+    int len = value.length();
+    pData = pCharacteristic->getData();
+    if (pData != NULL){
+      Serial.print("Notify callback for characteristic ");
+      Serial.print(pCharacteristic->getUUID().toString().c_str());
+      Serial.print(" of data length ");
+      Serial.println(len);
+      Serial.print("TX  ");
+      for (int i = 0; i < len; i++){
+        Serial.printf("%02X ", pData[i]);
+      }
+      Serial.println();
+      
+    }
+      
+  }
+};
+
+void initBLE(){
+  BBUILTINLEDevice::init("Esp32 Watch");
+  BLEServer *pServer = BBUILTINLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+  
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  pCharacteristicTX = pService->createCharacteristic(
+                                         CHARACTERISTIC_UUID_TX,
+                                         BLECharacteristic::PROPERTY_NOTIFY
+                                       );
+  pCharacteristicRX = pService->createCharacteristic(
+                                         CHARACTERISTIC_UUID_RX,
+                                         BLECharacteristic::PROPERTY_WRITE |
+                                         BLECharacteristic::PROPERTY_WRITE_NR
+                                       );
+  pCharacteristicRX->setCallbacks(new MyCallbacks());
+  pCharacteristicTX->addDescriptor(new BLE2902());
+  
+
+  pService->start();
+  // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
+  BLEAdvertising *pAdvertising = BBUILTINLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+  pAdvertising->setMinPreferred(0x12);
+  BBUILTINLEDevice::startAdvertising();
+  Serial.println("Characteristic defined! Now you can read it in your phone!");
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Starting BLE work!");
+  pinMode(BUILTINLED, OUTPUT);
+
+  initBLE();
+}
+
+void loop() {
+
+  if (deviceConnected){
+    digitalWrite(BUILTINLED, HIGH);
+    
+    if (Serial.available() > 0){
+      String dat = Serial.readString();
+      if (dat == "BAT"){
+        pCharacteristicTX->setValue(bat80, 8);
+        pCharacteristicTX->notify();
+        Serial.println("Sent Battery level");
+      }
+    }
+  } else {
+    digitalWrite(BUILTINLED, LOW);
+  }
+    
+  delay(10);
+}
+
+
+
+
